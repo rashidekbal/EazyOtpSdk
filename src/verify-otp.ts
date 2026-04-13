48fee07970e5806f1e4e3ec1483d91b7ced78797
Rasid Ekbal <rtechdevlopment123@gmail.com>
2026-04-14 00:52:37 +0530

    fix verify otp

--- src/verify-otp.ts
+++ src/verify-otp.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { verifyUrl } from "./constants.js";
 import formUrl from "./utils/formUrl.js";
+import { isValidEmail } from "./utils/regex.js";
 
 export default async function verifyOtp(email:string,otp:number,apiKey:string){
     return new Promise(async(resolve,reject)=>{
         if(!email||!otp||!apiKey)return reject("invalid parameters");
+        if(!isValidEmail(email))return reject("invalid email");
         const Url=formUrl(verifyUrl,apiKey);
         try {
             await axios.post(Url,{email:email,otp:otp});
